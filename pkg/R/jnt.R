fea166421a637179e8cfda63455d7a158c4e38c6
kmiddleton <kmiddleton@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-03 17:47:53 +0000

    More options for plot.jnt().
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/jnt@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/jnt.R
+++ pkg/R/jnt.R
@@ -1,7 +1,7 @@
 #' Johnson-Neyman Technique
+#' 
 #' This function performs the Johnson-Neyman Technique on data contained in two
 #' \code{data.frame}s.
-#' 
 #' 
 #' @aliases jnt jnt.data.frame print.jnt
 #' 
@@ -22,7 +22,7 @@
 #'   \item{lower}{Lower edge  of range of no significant different in slopes}
 #'   \item{upper}{Upper edge of range of no significant different in slopes}
 #' 
-#' @author Kevin Middleton (kmm@@csusb.edu)
+#' @author Kevin Middleton (\email{kmm@@csusb.edu})
 #' 
 #' @references Johnson PO, Neyman J (1936) Tests of certain linear hypotheses
 #'   and their application to some educational problems. \emph{Statistical
@@ -72,85 +72,86 @@
 #' 
 #' plot(jnt.out)
 #' 
-jnt <- function(dat1, dat2 = NULL, which.is.fact = NULL, alpha = 0.05, total.comp = 1)
-{
+jnt <- function(dat1, dat2 = NULL, which.is.fact = NULL, alpha = 0.05, total.comp = 1) {
   UseMethod("jnt")
 }
 
-jnt.data.frame <- function(dat1, dat2 = NULL, which.is.fact = NULL, alpha = 0.05, total.comp = 1)
-{
-dat1.n <- nrow(dat1)
-dat2.n <- nrow(dat2)
-total.n <- dat1.n + dat2.n			# Total n for both groups
-indiv.res.df <- total.n - 4			# Individual residual df
-within.res.df <- total.n - 2 - 1	# Within residual df
+jnt.data.frame <- function(dat1, dat2 = NULL, which.is.fact = NULL, alpha = 0.05, total.comp = 1) {
+  dat1.n <- nrow(dat1)
+  dat2.n <- nrow(dat2)
+  total.n <- dat1.n + dat2.n              # Total n for both groups
+  indiv.res.df <- total.n - 4             # Individual residual df
+  within.res.df <- total.n - 2 - 1        # Within residual df
 
-dat1.x <- dat1[,1]
-dat1.y <- dat1[,2]
-dat1.xbar <- mean(dat1.x)
-dat1.ybar <- mean(dat1.y)
-dat1.x.sd <- sd(dat1[,1])
-dat1.sum.x <- sum(dat1.x)
-dat1.sum.y <- sum(dat1.y)
-dat1.sum.x2 <- sum(dat1.x^2) - (dat1.sum.x^2 / dat1.n)
-dat1.sum.y2 <- sum(dat1.y^2) - (dat1.sum.y^2 / dat1.n)
-dat1.sum.xy <- sum(dat1.x * dat1.y) - sum(dat1.x) * sum(dat1.y) / dat1.n
-dat1.X <- matrix(c(rep(1, times = dat1.n), dat1.x), ncol = 2)
-dat1.fit <- (solve(t(dat1.X) %*% dat1.X)) %*% (t(dat1.X) %*% dat1[,2])
-dat1.int <- dat1.fit[1]
-dat1.slope <- dat1.fit[2]
+  dat1.x <- dat1[,1]
+  dat1.y <- dat1[,2]
+  dat1.xbar <- mean(dat1.x)
+  dat1.ybar <- mean(dat1.y)
+  dat1.x.sd <- sd(dat1[,1])
+  dat1.sum.x <- sum(dat1.x)
+  dat1.sum.y <- sum(dat1.y)
+  dat1.sum.x2 <- sum(dat1.x^2) - (dat1.sum.x^2 / dat1.n)
+  dat1.sum.y2 <- sum(dat1.y^2) - (dat1.sum.y^2 / dat1.n)
+  dat1.sum.xy <- sum(dat1.x * dat1.y) - sum(dat1.x) * sum(dat1.y) / dat1.n
+  dat1.X <- matrix(c(rep(1, times = dat1.n), dat1.x), ncol = 2)
+  dat1.fit <- (solve(t(dat1.X) %*% dat1.X)) %*% (t(dat1.X) %*% dat1[,2])
+  dat1.int <- dat1.fit[1]
+  dat1.slope <- dat1.fit[2]
 
-dat2.x <- dat2[,1]
-dat2.y <- dat2[,2]
-dat2.xbar <- mean(dat2.x)
-dat2.ybar <- mean(dat2.y)
-dat2.x.sd <- sd(dat2[,1])
-dat2.sum.x <- sum(dat2.x)
-dat2.sum.y <- sum(dat2.y)
-dat2.sum.x2 <- sum(dat2.x^2) - (dat2.sum.x^2 / dat2.n)
-dat2.sum.y2 <- sum(dat2.y^2) - (dat2.sum.y^2 / dat2.n)
-dat2.sum.xy <- sum(dat2.x * dat2.y) - sum(dat2.x) * sum(dat2.y) / dat2.n
-dat2.X <- matrix(c(rep(1, times = dat2.n), dat2.x), ncol = 2)
-dat2.fit <- (solve(t(dat2.X) %*% dat2.X)) %*% (t(dat2.X) %*% dat2[,2])
-dat2.int <- dat2.fit[1]
-dat2.slope <- dat2.fit[2]
+  dat2.x <- dat2[,1]
+  dat2.y <- dat2[,2]
+  dat2.xbar <- mean(dat2.x)
+  dat2.ybar <- mean(dat2.y)
+  dat2.x.sd <- sd(dat2[,1])
+  dat2.sum.x <- sum(dat2.x)
+  dat2.sum.y <- sum(dat2.y)
+  dat2.sum.x2 <- sum(dat2.x^2) - (dat2.sum.x^2 / dat2.n)
+  dat2.sum.y2 <- sum(dat2.y^2) - (dat2.sum.y^2 / dat2.n)
+  dat2.sum.xy <- sum(dat2.x * dat2.y) - sum(dat2.x) * sum(dat2.y) / dat2.n
+  dat2.X <- matrix(c(rep(1, times = dat2.n), dat2.x), ncol = 2)
+  dat2.fit <- (solve(t(dat2.X) %*% dat2.X)) %*% (t(dat2.X) %*% dat2[,2])
+  dat2.int <- dat2.fit[1]
+  dat2.slope <- dat2.fit[2]
 
-f.crit <- qf((1-alpha / total.comp / 2), 1, indiv.res.df)
+  f.crit <- qf((1-alpha / total.comp / 2), 1, indiv.res.df)
 
-SSresi <- ((dat1.sum.y2 - ((dat1.sum.xy^2 / dat1.sum.x2)))) + 
-	((dat2.sum.y2 - ((dat2.sum.xy^2 / dat2.sum.x2))))
+  SSresi <- ((dat1.sum.y2 - ((dat1.sum.xy^2 / dat1.sum.x2)))) + 
+    ((dat2.sum.y2 - ((dat2.sum.xy^2 / dat2.sum.x2))))
 
-SSresw <- (dat1.sum.y2 + dat2.sum.y2) - 
-	((dat1.sum.xy + dat2.sum.xy)^2 / (dat1.sum.x2 + dat2.sum.x2))
+  SSresw <- (dat1.sum.y2 + dat2.sum.y2) - 
+    ((dat1.sum.xy + dat2.sum.xy)^2 / (dat1.sum.x2 + dat2.sum.x2))
 
-total.ss <- sum(c(sum(dat1.y^2), sum(dat2.y^2))) - sum(c(dat1.y, dat2.y))^2 / total.n
+  total.ss <- sum(c(sum(dat1.y^2), sum(dat2.y^2))) - sum(c(dat1.y, dat2.y))^2 / total.n
 
-sum.xys <- sum(c(dat1.x * dat1.y, dat2.x * dat2.y))
-sum.xs <- sum(c(dat1.x, dat2.x))
-sum.ys <- sum(c(dat1.y, dat2.y))
-sum.x2s <- sum(c(dat1.x^2, dat2.x^2))
+  sum.xys <- sum(c(dat1.x * dat1.y, dat2.x * dat2.y))
+  sum.xs <- sum(c(dat1.x, dat2.x))
+  sum.ys <- sum(c(dat1.y, dat2.y))
+  sum.x2s <- sum(c(dat1.x^2, dat2.x^2))
 
-SSrest <- total.ss - (((sum.xys - sum.xs * sum.ys / total.n)^2) / (sum.x2s - (sum.xs^2 / total.n)))
+  SSrest <- total.ss - (((sum.xys - sum.xs * sum.ys / total.n)^2) / (sum.x2s - (sum.xs^2 / total.n)))
 
-SSregw <- (dat1.sum.xy + dat2.sum.xy)^2 / (dat1.sum.x2 + dat2.sum.x2)
+  SSregw <- (dat1.sum.xy + dat2.sum.xy)^2 / (dat1.sum.x2 + dat2.sum.x2)
 
-bw <- (dat1.sum.xy + dat2.sum.xy) / (dat1.sum.x2 + dat2.sum.x2)
+  bw <- (dat1.sum.xy + dat2.sum.xy) / (dat1.sum.x2 + dat2.sum.x2)
 
-A.val <- (-f.crit / indiv.res.df) * SSresi * ((1/dat1.sum.x2) + (1/dat2.sum.x2)) + 
-	(dat1.slope - dat2.slope)^2
+  A.val <- (-f.crit / indiv.res.df) * SSresi * ((1/dat1.sum.x2) + (1/dat2.sum.x2)) + 
+    (dat1.slope - dat2.slope)^2
 
-B.val <- (f.crit / indiv.res.df) * SSresi * ((dat1.xbar/dat1.sum.x2) + (dat2.xbar/dat2.sum.x2)) +
-	(dat1.int - dat2.int) * (dat1.slope - dat2.slope)
+  B.val <- (f.crit / indiv.res.df) * SSresi * ((dat1.xbar/dat1.sum.x2) + (dat2.xbar/dat2.sum.x2)) +
+    (dat1.int - dat2.int) * (dat1.slope - dat2.slope)
 
-C.val <- (-f.crit/indiv.res.df) * SSresi * (total.n / (dat1.n * dat2.n) + dat1.xbar^2/dat1.sum.x2 +
-	dat2.xbar^2 / dat2.sum.x2) + (dat1.int - dat2.int)^2
+  C.val <- (-f.crit/indiv.res.df) * SSresi * (total.n / (dat1.n * dat2.n) + dat1.xbar^2/dat1.sum.x2 +
+                                              dat2.xbar^2 / dat2.sum.x2) + (dat1.int - dat2.int)^2
 
-lower <- (-B.val - sqrt(B.val^2 - A.val*C.val))/A.val
-upper <- (-B.val + sqrt(B.val^2 - A.val*C.val))/A.val
+  lower <- (-B.val - sqrt(B.val^2 - A.val*C.val))/A.val
+  upper <- (-B.val + sqrt(B.val^2 - A.val*C.val))/A.val
 
-jntobj <- list("dat1" = data.frame(x = dat1.x, y = dat1.y), "dat2" = data.frame(x = dat2.x, y = dat2.y), 
-	"alpha" = alpha, "slope1" = dat1.slope, "int1" = dat1.int, "slope2" = dat2.slope, "int2" = dat2.int,
-	"lower" = lower, "upper" = upper)
-class(jntobj) <- 'jnt'
-jntobj
+  jntobj <- list("dat1" = data.frame(x = dat1.x, y = dat1.y),
+                 "dat2" = data.frame(x = dat2.x, y = dat2.y), 
+                 "alpha" = alpha,
+                 "slope1" = dat1.slope, "int1" = dat1.int,
+                 "slope2" = dat2.slope, "int2" = dat2.int,
+                 "lower" = lower, "upper" = upper)
+  class(jntobj) <- 'jnt'
+  jntobj
 }
--- pkg/R/plot.jnt.R
+++ pkg/R/plot.jnt.R
@@ -2,16 +2,18 @@
 #' 
 #' Plot object returned by \code{jnt}.
 #' 
-#' 
 #' @method plot jnt
 #' 
 #' @param x Object of class \code{jnt}
 #' @param reg.lty Line type for regression lines. Defaults to 1.
-#' @param jnt.lty Line type for range of significance. Defaults to 2.
-#' @param plot.range logical. If \code{TRUE} (the default) the range of significance is plotted.
-#' @param ... Additional parameters passed to plot
+#' @param jnt.lty Line type for range of insignificant differences. Defaults to 2.
+#' @param col.1 Color for series 1. Defaults to "black."
+#' @param col.2 Color for series 2. Defaults to "gray."
+#' @param col.signif Color for bars denoting range of insignificance. Defaults to "red."
+#' @param plot.range logical. If \code{TRUE} (the default) the range of insignificance is plotted.
+#' @param ... Additional parameters passed to plot (e.g., lwd, pch, xlab, ylab).
 #' 
-#' @author Kevin Middleton (kmm@@csusb.edu)
+#' @author Kevin Middleton (\email{kmm@@csusb.edu})
 #' 
 #' @keywords univar
 #' 
@@ -19,25 +21,36 @@
 #' 
 #' example(jnt)
 #' 
-plot.jnt <- function(x, reg.lty = 1, jnt.lty = 2, plot.range = TRUE, ...){
-  reg.lty <- 1
-  jnt.lty <- 2
+plot.jnt <- function(x,
+                     reg.lty = 1, jnt.lty = 2,
+                     col.1 = "black", col.2 = "gray",
+                     col.signif = "red",
+                     plot.range = TRUE, ...){
+  if (!inherits(x, "jnt")) {
+    stop('object "x" is not of class "jnt".')
+  }
 
+  dots <- list(...)
+  if("lwd" %in% names(dots)) {lwd <- dots$lwd} else {lwd <- 1}
+  if("pch" %in% names(dots)) {pch <- dots$pch} else {pch <- 16}
+  if("xlab" %in% names(dots)) {xlab <- dots$xlab} else {xlab <- "x"}
+  if("ylab" %in% names(dots)) {ylab <- dots$ylab} else {xlab <- "y"}
+  
   x1 <- x$dat1$x
   x2 <- x$dat2$x
   y1 <- x$dat1$y
   y2 <- x$dat2$y
 
-  plot(c(x1, x2), c(y1, y2), type = "n", xlab = "x", ylab = "y")
+  plot(c(x1, x2), c(y1, y2), type = "n", xlab = xlab, ylab = ylab, ...)
 
   if (plot.range) {
-    abline(v = x$lower, lty = jnt.lty, col = "red", ...)
-    abline(v = x$upper, lty = jnt.lty, col = "red", ...)
+    abline(v = x$lower, lty = jnt.lty, col = col.signif, lwd = lwd)
+    abline(v = x$upper, lty = jnt.lty, col = col.signif, lwd = lwd)
   }
 
-  abline(x$int1, x$slope1, lty = reg.lty)
-  abline(x$int2, x$slope2, lty = reg.lty)
+  abline(x$int1, x$slope1, lty = reg.lty, lwd = lwd)
+  abline(x$int2, x$slope2, lty = reg.lty, lwd = lwd)
 
-  points(x1, y1, pch = 16, col = "grey")
-  points(x2, y2, pch = 16)	
+  points(x1, y1, pch = pch, col = col.1)
+  points(x2, y2, pch = pch, col = col.2)	
 }
