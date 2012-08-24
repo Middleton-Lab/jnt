7a3f765e144812f50d8299ee506425cb6e41d464
Kevin Middleton <kmm@csusb.edu>
2012-08-24 16:54:23 -0500

    Version bump 0.2-1

--- R/jnt-package.R
+++ R/jnt-package.R
@@ -5,8 +5,8 @@
 ##' \tabular{ll}{
 ##' Package: \tab jnt\cr
 ##' Type: \tab Package\cr
-##' Version: \tab 0.1-14\cr
-##' Date: \tab 2011-10-06\cr
+##' Version: \tab 0.2-1\cr
+##' Date: \tab 2012-08-24\cr
 ##' License: \tab GPL-2\cr
 ##' LazyLoad: \tab yes\cr
 ##' LazyData: \tab yes\cr
