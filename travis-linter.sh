60282c4cb2323a2144705115b810bb9b8f95e4fc
Kevin Middleton <middletonk@missouri.edu>
2015-10-18 10:36:25 -0500

    Fix travis-linter script.

--- travis-linter.sh
+++ travis-linter.sh
@@ -3,7 +3,7 @@ set -e
 
 exitstatus=0
 
-for file in *.R
+for file in $(find ./R -name '*.R')
 do
     Rscript -e "lintr::lint(\"$file\")"
     outputbytes=`Rscript -e "lintr::lint(\"$file\")" | grep ^ | wc -c`
