f62f81373bf0fc83e987a513ef0704a119b007a8
Kevin Middleton <middletonk@missouri.edu>
2016-02-04 09:22:49 -0600

    Fix errors in processing Lavin data
    Rename incoming columns of data. Should think about restoring the name
    on output.

--- R/jnt-package.R
+++ R/jnt-package.R
@@ -1,7 +1,7 @@
 ##' Johnson-Neyman Technique
-##' 
+##'
 ##' Carry out the Johnson-Neyman Technique
-##' 
+##'
 ##' \tabular{ll}{
 ##' Package: \tab jnt\cr
 ##' Type: \tab Package\cr
@@ -11,48 +11,48 @@
 ##' LazyLoad: \tab yes\cr
 ##' LazyData: \tab yes\cr
 ##' }
-##' 
+##'
 ##' @name jnt-package
-##' 
+##'
 ##' @docType package
-##' 
+##'
 ##' @author Kevin Middleton (middletonk@@missouri.edu)
-##' 
+##'
 ##' Maintainer: Kevin Middleton (middletonk@@missouri.edu)
 ##'
 ##' @references Johnson PO, Neyman J (1936) Tests of certain linear
 ##'   hypotheses and their application to some educational problems.
 ##'   \emph{Statistical Research Memoirs} 1:57-93.
-##'   
+##'
 ##'   Hunka S, Leighton J (1997) Defining Johnson-Neyman regions of
 ##'   significance in three-covariate ANCOVA using Mathematica.
 ##'   \emph{Journal of Educational and Behavioral Statistics} 22:
 ##'   361-387.
-##'   
+##'
 ##'   White CR (2003) Allometric analysis beyond heterogenous
 ##'   regression slopes: Use of the Johnson-Neyman Technique in
 ##'   comparative biology. \emph{Physiol Biochem Zool} 76: 135-140.
-##' 
+##'
 ##' \emph{Examples:}
-##' 
+##'
 ##' White CR (2003) The influence of foraging mode and arid adaptation
 ##' on the basal metabolic rates of burrowing mammals. \emph{Physiol
 ##' Biochem Zool} 76: 122-134.
-##' 
+##'
 ##' Lavin SR, Karasov WH, Ives AR, Middleton KM, Garland T, Jr (2008)
 ##' Morphometrics of the avian small intestine compared with that of
 ##' nonflying mammals: A phylogenetic approach. \emph{Physiol Biochem
 ##' Zool} 81: 526-550.
 ##'
 ##' @keywords package
-##' 
+##'
 ##' @importFrom graphics abline plot points
 ##' @importFrom stats coef lm qf sd
 ##'
 ##' @examples
-##' 
+##'
 ##' example(jnt)
-##' 
+##'
 NULL
 
 
@@ -60,17 +60,17 @@ NULL
 
 
 ##' Data from Lavin et al., 2008
-##' 
+##'
 ##' Data set 1 from Lavin et al., 2008
-##' 
-##' 
+##'
+##'
 ##' @name Lavin.1
 ##'
 ##' @aliases Lavin.1 Lavin.2
 ##'
 ##' @docType data
 ##'
-##' @format A data frame with 64 observations on the following 2 
+##' @format A data frame with 64 observations on the following 2
 ##'   variables.
 ##'   \describe{
 ##'   \item{\code{V1}}{a numeric vector}
@@ -84,14 +84,16 @@ NULL
 ##'
 ##' @keywords datasets
 ##'
+##' @import smatr
+##'
 ##' @examples
-##' 
+##'
 ##' str(Lavin.1)
 ##' str(Lavin.2)
-##' 
+##'
 ##' (Lavin <- jnt(Lavin.1, Lavin.2))
 ##' plot(Lavin)
-##' 
+##'
 NULL
 
 
@@ -99,23 +101,23 @@ NULL
 
 
 ##' Data from White, 2003
-##' 
+##'
 ##' Data set 1 from from White, 2003
-##' 
-##' 
+##'
+##'
 ##' @name White.1
 ##'
 ##' @aliases White.1 White.2
-##' 
+##'
 ##' @docType data
 ##'
-##' @format A data frame with 50 observations on the following 2 
+##' @format A data frame with 50 observations on the following 2
 ##'   variables.
 ##'   \describe{
 ##'   \item{\code{V1}}{a numeric vector}
 ##'   \item{\code{V2}}{a numeric vector}
 ##' }
-##' 
+##'
 ##' @references White CR (2003) Allometric analysis beyond
 ##'   heterogenous regression slopes: Use of the Johnson-Neyman
 ##'   Technique in comparative biology. \emph{Physiol Biochem Zool}
@@ -124,13 +126,13 @@ NULL
 ##' @keywords datasets
 ##'
 ##' @examples
-##' 
+##'
 ##' str(White.1)
 ##' str(White.2)
-##' 
+##'
 ##' (White <- jnt(White.1, White.2))
 ##' plot(White)
-##' 
+##'
 NULL
 
 
--- R/jnt.R
+++ R/jnt.R
@@ -6,8 +6,6 @@
 ##'
 ##' @title Johnson-Neyman Technique
 ##'
-##' @aliases jnt print.jnt
-##'
 ##' @param dat1 \code{data.frame} containing data set 1
 ##' @param dat2 \code{data.frame} containing data set 2
 ##' @param which.is.fact Currently not implemented
@@ -88,20 +86,24 @@ jnt <- function(dat1,
                 alpha = 0.05,
                 total.comp = 1,
                 use_sma = FALSE) {
-  
+
   if (use_sma) {
-    require(smatr)
     message("Fitting with SMA.")
     fit_method <- "SMA"
   } else {
     message("Fitting with OLS")
     fit_method <- "OLS"
   }
-  
+
   # Convert to data.frames to avoid problems with tbl_df
   dat1 <- as.data.frame(dat1)
   dat2 <- as.data.frame(dat2)
-  
+
+  # Rename column variables to x and y
+  message("Assuming x variable is column 1, and y is column 2.")
+  names(dat1) <- c("x", "y")
+  names(dat2) <- c("x", "y")
+
   ## Test for significant slope difference
   not.signif <- precheck(dat1, dat2)
   if (not.signif) {
@@ -109,7 +111,7 @@ jnt <- function(dat1,
          You probably should reconsider using
          the Johnson-Neyman Technique.")
   }
-  
+
   if (use_sma) {
     fm1 <- sma(y ~ x, data = dat1, method = fit_method)
     fm2 <- sma(y ~ x, data = dat2, method = fit_method)
@@ -117,22 +119,22 @@ jnt <- function(dat1,
     fm1 <- lm(dat1$y ~ dat1$x)
     fm2 <- lm(dat2$y ~ dat2$x)
   }
-  
+
   a1 <- as.numeric(coef(fm1))[1]
   b1 <- as.numeric(coef(fm1))[2]
-  
+
   a2 <- as.numeric(coef(fm2))[1]
   b2 <- as.numeric(coef(fm2))[2]
-  
+
   n1 <- nrow(dat1)
   n2 <- nrow(dat2)
   N <- n1 + n2
   f_crit <- qf((1 - alpha / 2), 1, N - 4)
-  
+
   ss_resid <- sum(residuals(fm1) ^ 2) + sum(residuals(fm2) ^ 2)
   ss_x1 <- sum((dat1$x - mean(dat1$x)) ^ 2)
   ss_x2 <- sum((dat2$x - mean(dat2$x)) ^ 2)
-  
+
   A <- (-f_crit / (N - 4)) * ss_resid * (1/ss_x1 + 1/ss_x2) + (b1 - b2) ^ 2
   B <- (f_crit / (N - 4)) * ss_resid * (mean(dat1$x)/ss_x1 + mean(dat2$x/ss_x2)) +
     (a1 - a2) * (b1 - b2)
@@ -140,12 +142,12 @@ jnt <- function(dat1,
     (N / (n1 * n2) +
        mean(dat1$x) ^ 2 / ss_x1 +
        mean(dat2$x) ^ 2 / ss_x2) + (a1 - a2) ^ 2
-  
+
   lower <- (-B - sqrt(B ^ 2 - A * C)) / A
   upper <- (-B + sqrt(B ^ 2 - A * C)) / A
-  
-  jntobj <- list("dat1" = data.frame(x = dat1.x, y = dat1.y),
-                 "dat2" = data.frame(x = dat2.x, y = dat2.y),
+
+  jntobj <- list("dat1" = data.frame(x = dat1$x, y = dat1$y),
+                 "dat2" = data.frame(x = dat2$x, y = dat2$y),
                  "alpha" = alpha,
                  "slope1" = b1,
                  "int1" = a1,
@@ -157,6 +159,7 @@ jnt <- function(dat1,
   return(jntobj)
 }
 
+##' @export
 print.jnt <- function(x, digits = 4, ...){
   cat("\n")
   cat("Johnson-Neyman Technique\n")
@@ -172,9 +175,8 @@ print.jnt <- function(x, digits = 4, ...){
   cat("\tIntercept\t", format(x$int2, digits = digits), "\n")
   cat("\n")
   cat("Region of non-significant slope difference\n")
-  cat("\tLower\t\tUpper\n")
-  cat("\t", format(x$lower, digits = digits), "\t",
-      format(x$upper, digits = digits), "\n\n")
+  cat("\tLower:\t", format(x$lower, digits = digits), "\n")
+  cat("\tUpper:", format(x$upper, digits = digits), "\n\n")
 }
 
 ## Check for no slope difference
